29add7512bbbe78609e6e3379cc22ac65b8ce4de
Maleesha dilukshan <133895520+matheeshadunal@users.noreply.github.com>
2024-10-08 21:20:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put sesson id",
+SESSION_ID: process.env.SESSION_ID || "Od8VECYb#NIMkTs6nHOnNyeCZLoWHXDhZxLtjm572f0mWQl8qJzg",
 SUDO_NB: process.env.SUDO_NB || "94767910958",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_BIO: process.env.AUTO_BIO || "false",
@@ -13,5 +13,5 @@ ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "unavailable",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "false",
 AUTO_TYPING:"true",
 MODE: process.env.MODE || "public",
-AUTO_VOICE:"true",
+AUTO_VOICE:"false",
 };
